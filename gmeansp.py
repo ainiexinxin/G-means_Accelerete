247ae750c1d2ac821d9b89874cddd81e8f6e0d15
chenchenchen <117@kunkun.com>
2024-04-15 11:34:21 +0800

    final ver

--- gmeansp.py
+++ gmeansp.py
@@ -9,6 +9,12 @@ from pdb import set_trace
 from model import *
 import time
 
+seed_num = 17
+random_state = 1010
+strictness_num = 4
+exmple_nums = 10000
+features_nums = 30
+centers_nums = 30
 
 def seed_globe(seed):
 	np.random.seed(seed)
@@ -17,44 +23,40 @@ def seed_globe(seed):
 
 def drain(plot_data):
 	title = ['best_cluster', 'gmeans', 'gmeansAcc']
-	fig, axis = plt.subplots(nrows=1, ncols=3, figsize=(12, 3))
+	fig, axis = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
 
 	for i in range(3):
 		sbn.scatterplot(x='x', y='y', data=plot_data, hue=title[i], ax=axis[i], palette='tab20_r')
+		
 		axis[i].set_title(title[i])
 		plt.subplots_adjust(wspace=0.3, hspace=0.3)
 
 	plt.show()
-	set_trace()
 
 
 if __name__ == '__main__':
-	seed_globe(17)
+	seed_globe(seed_num)
 	# iris = datasets.load_iris().data
 
-	iris, labels = datasets.make_blobs(n_samples=1000,
-		n_features=5,
-		centers=5)
+	iris, labels = datasets.make_blobs(n_samples=exmple_nums,
+		n_features=features_nums,
+		centers=centers_nums)
 
-	gmeans = GMeans_MB(random_state=1010,
-					   strictness=4)
+	gmeans = GMeans_MB(random_state=random_state,
+					   strictness=strictness_num)
 
-	gmeansAcc = GMeans_Acc(random_state=1010,
-					   strictness=4)
+	gmeansAcc = GMeans_Acc(random_state=random_state,
+					   strictness=strictness_num)
 
-
-	start = time.time()
+	start1 = time.time()
 	gmeans.fit(iris)
-	end = time.time()
+	end1 = time.time()
 
-	print("[G_means with minbatchkmeans] Time Elapsed:", end - start)
 
 	# G-means with Acc
 	start = time.time()
 	gmeansAcc.fit(iris)
 	end = time.time()
-
-	print("[G_meansAcc] Time Elapsed:", end - start)
 
 	# 画图
 	plot_data = pd.DataFrame(iris[:, 0:2])
@@ -64,4 +66,8 @@ if __name__ == '__main__':
 	plot_data['gmeans'] = gmeans.labels_
 	plot_data['gmeansAcc'] = gmeansAcc.labels_
 
+
+	print("[G_means with minbatchkmeans] Time Elapsed:", end1 - start1)
+	print("[G_meansAcc] Time Elapsed:", end - start)
 	drain(plot_data)
+
